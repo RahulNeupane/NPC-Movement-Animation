196710607179b3556590604786d2d3c3ff0c403b
RahulNeupane <nrahul503@gmail.com>
2023-02-06 20:13:48 +0545

    random chaotic jumps

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /** @type{HTMLCanvasElement} */ 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-const totalEnemies = 100
+const totalEnemies = 10
 let gameFrame = 0
 const enemyArray = []
 
@@ -19,19 +19,20 @@ class Enemy {
     constructor(){
         this.image = new Image()
         this.image.src = 'enemy1.png'
-        this.x = Math.random() * canvas.width,
-        this.y = Math.random()*canvas.height,
-        this.speed = Math.random() * 4 - 2
+        // this.speed = Math.random() * 4 - 2
         this.spriteWidth = 293
         this.spriteHeight = 155
         this.width = this.spriteWidth /2
         this.height = this.spriteHeight /2
+        this.x = Math.random() * (canvas.width - this.width),
+        this.y = Math.random()*(canvas.height - this.height),
+
         this.frame = 0
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
     }
     update(){
-        this.x+= this.speed
-        this.y+= this.speed
+        this.x+= Math.random() * 5 - 2.5
+        this.y+= Math.random() * 5 - 2.5
         if(gameFrame%this.flapSpeed==0){
             this.frame>4 ? this.frame=0 : this.frame++
         }
